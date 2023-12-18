4b6e48ec108ef3bc97a50bd558bb1e50f06cf848
Kirill Mikulich <k.mikulich@linker.by>
2023-12-18 15:05:55 +0300

    add secondary style for input

--- src/stories/inputs/Inputs.stories.tsx
+++ src/stories/inputs/Inputs.stories.tsx
@@ -1,108 +1,108 @@
-import { Meta, StoryObj } from "@storybook/react";
-import TextField, { TextFieldProps } from "components/Controls/TextField";
-import React from "react";
+import { Meta, StoryObj } from '@storybook/react';
+import TextField, { TextFieldProps } from 'components/Controls/TextField';
+import React from 'react';
 
 const meta: Meta<TextFieldProps> = {
-  title: 'Inputs',
-  component: TextField,
-  argTypes: {
-    variant: {
-      type: 'string',
-      description: 'Input Style',
-      defaultValue: 'primary',
-      options: ['primary', 'secondary'],
-      control: { type: 'select' }
-    },
-    label: {
-      type: 'string',
-      description: 'Label for input',
-      defaultValue: '',
-      control: { type: 'text' }
-    },
-    value: {
-      type: 'string',
-      description: 'Value in input',
-      defaultValue: '',
-      control: { type: 'text' },
-    },
-    disabled: {
-      type: 'boolean',
-      description: 'Disabled input',
-      defaultValue: false,
-      control: { type: 'boolean' },
-      if: { arg: 'error', truthy: false },
-    },
-    error: {
-      type: 'boolean',
-      description: 'Error style input',
-      defaultValue: false,
-      control: { type: 'boolean' },
-      if: { arg: 'disabled', truthy: false },
-    },
-    errorMessage: {
-      type: 'string',
-      description: 'Error message for input',
-      defaultValue: 'Error',
-      control: { type: 'text' },
-      if: { arg: 'error', truthy: true },
-    },
-    helperMessage: {
-      type: 'string',
-      description: 'Helper text for input',
-      defaultValue: 'Helper text',
-      control: { type: 'text' },
-    }
-  }
-}
+	title: 'Inputs',
+	component: TextField,
+	argTypes: {
+		variant: {
+			type: 'string',
+			description: 'Input Style',
+			defaultValue: 'primary',
+			options: ['primary', 'secondary'],
+			control: { type: 'select' },
+		},
+		label: {
+			type: 'string',
+			description: 'Label for input',
+			defaultValue: '',
+			control: { type: 'text' },
+		},
+		value: {
+			type: 'string',
+			description: 'Value in input',
+			defaultValue: '',
+			control: { type: 'text' },
+		},
+		disabled: {
+			type: 'boolean',
+			description: 'Disabled input',
+			defaultValue: false,
+			control: { type: 'boolean' },
+			if: { arg: 'error', truthy: false },
+		},
+		error: {
+			type: 'boolean',
+			description: 'Error style input',
+			defaultValue: false,
+			control: { type: 'boolean' },
+			if: { arg: 'disabled', truthy: false },
+		},
+		errorMessage: {
+			type: 'string',
+			description: 'Error message for input',
+			defaultValue: 'Error',
+			control: { type: 'text' },
+			if: { arg: 'error', truthy: true },
+		},
+		helperMessage: {
+			type: 'string',
+			description: 'Helper text for input',
+			defaultValue: 'Helper text',
+			control: { type: 'text' },
+		},
+	},
+};
 
 export default meta;
 
 type Story = StoryObj<TextFieldProps>;
 
 export const Standart: Story = {
-  args: {
-    variant: 'primary',
-    label: 'Lorem ipsum',
-    value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-  },
-  render: (args) => <TextField {...args} sx={{maxWidth: '200px'}}/>
-}
+	args: {
+		variant: 'primary',
+		label: 'Lorem ipsum',
+		value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+	},
+	render: args => <TextField {...args} />,
+};
 
 export const LongTitle: Story = {
-  args: {
-    variant: 'primary',
-    label: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-  },
-  render: (args) => <TextField {...args} sx={{maxWidth: '200px'}}/>
-}
+	args: {
+		variant: 'primary',
+		label: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+	},
+	render: args => <TextField {...args} />,
+};
 
 export const Error: Story = {
-  args: {
-    variant: 'primary',
-    label: 'Lorem ipsum',
-    error: true,
-    errorMessage: 'Test error text',
-    value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit'
-  },
-  render: (args) => <TextField {...args} sx={{maxWidth: '200px'}}/>
-}
+	args: {
+		variant: 'primary',
+		label: 'Lorem ipsum',
+		error: true,
+		errorMessage: 'Test error text',
+		value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+	},
+	render: args => <TextField {...args} />,
+};
 
 export const HelperText: Story = {
-  args: {
-    variant: 'primary',
-    label: 'Lorem ipsum',
-    helperMessage: 'Lorem ipsum dolor sit',
-    value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit'
-  },
-  render: (args) => <TextField {...args} sx={{maxWidth: '200px'}}/>
-}
+	args: {
+		variant: 'primary',
+		label: 'Lorem ipsum',
+		helperMessage: 'Lorem ipsum dolor sit',
+		value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+	},
+	render: args => <TextField {...args} />,
+};
 
 export const DisabledReadOnly: Story = {
-  args: {
-    variant: 'primary',
-    label: 'Lorem ipsum',
-    disabled: true,
-    value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit'
-  },
-  render: (args) => <TextField {...args} sx={{maxWidth: '200px'}}/>
-}
+	args: {
+		variant: 'primary',
+		label: 'Lorem ipsum',
+		disabled: true,
+		value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+	},
+	render: args => <TextField {...args} />,
+};
--- src/styles/material/inputs/Secondary.tsx
+++ src/styles/material/inputs/Secondary.tsx
@@ -1,6 +1,123 @@
-import styled from "@emotion/styled";
-import { TextField } from "@mui/material";
+import styled from '@emotion/styled';
+import { TextField, TextFieldProps, Box } from '@mui/material';
+import React, { useState, type FC } from 'react';
+import {
+	BLUE_400,
+	GREY_200,
+	GREY_500,
+	GREY_700,
+	GREY_900,
+	RED_100,
+} from 'styles/constants';
 
-export const Secondary = styled(TextField)`
+const Field = styled(TextField)({
+	flex: '1 1 50%',
+	margin: '0',
+	padding: '0',
+	'.MuiFormLabel-root': {
+		display: 'none',
+	},
+	'.MuiInputBase-root': {
+		display: 'flex',
+		padding: '16px 8px',
+		margin: '0',
+		flexDirection: 'column',
+		justifyContent: 'end',
+		borderRadius: '0px',
+		backgroundColor: 'white',
+		minHeight: '53px',
+		caretColor: BLUE_400,
+		transition: '0.3s linear',
+		borderLeft: `1px solid transparent`,
+		borderTop: `1px solid transparent`,
+		borderRight: `1px solid transparent`,
+		'&:before': {
+			content: 'none',
+		},
+		'&:after': {
+			content: 'none',
+		},
+		'&:hover': {
+			borderRadius: '0px',
+			backgroundColor: GREY_200,
+		},
+		'&.Mui-focused': {
+			borderRadius: '0px',
+			backgroundColor: GREY_200,
+			borderLeft: `1px solid ${GREY_700}`,
+			borderTop: `1px solid ${GREY_700}`,
+			borderRight: `1px solid ${GREY_700}`,
+		},
+		'&.Mui-error': {
+			backgroundColor: RED_100,
+		},
+		'&.Mui-disabled': {
+			WebkitTextFillColor: 'none',
+			backgroundColor: 'transparent',
+		},
+	},
+	'.MuiInputBase-input': {
+		color: GREY_900,
+		padding: '0',
+		height: '17px',
+		fontSize: '14px',
+		fontStyle: 'normal',
+		fontWeight: '400',
+		lineHeight: '120%',
+	},
+	'.MuiOutlinedInput-notchedOutline': {
+		border: 'none',
+	},
+	'.MuiFormHelperText-root': {
+		margin: '4px 0 0 0',
+		padding: '0 8px',
+		color: GREY_500,
+		fontSize: '12px',
+		fontStyle: 'normal',
+		fontWeight: '300',
+		lineHeight: 'normal',
+	},
+});
 
+const Container = styled(Box)<{ focus?: boolean }>(({ focus }) => ({
+	display: 'flex',
+	flexDirection: 'row',
+	justifyContent: 'start',
+	transition: '0.3s linear',
+	borderBottom: `1px solid ${focus ? GREY_700 : 'transparent'}`,
+	'&:hover': {
+		borderBottom: `1px solid ${GREY_700}`,
+	},
+}));
+
+const Label = styled(Box)`
+	max-width: 234px;
+	flex: 1 1 50%;
+	display: flex;
+	align-items: center;
+	font-family: Inter;
+	color: ${GREY_900};
+	font-size: 14px;
+	font-style: normal;
+	font-weight: 700;
+	line-height: normal;
+	letter-spacing: 0.28px;
 `;
+
+export type Props = TextFieldProps;
+
+export const Secondary: FC<Props> = props => {
+	const { label, ...rest } = props;
+	const [focus, setFocus] = useState<boolean>(false);
+
+	return (
+		<Container focus={focus}>
+			<Label>{label}</Label>
+			<Field
+				onFocus={() => setFocus(true)}
+				onBlur={() => setFocus(false)}
+				{...rest}
+			></Field>
+		</Container>
+	);
+};
